02fe31a4eba54104702d4eca8c5d12b4c983e5c9
paulo9241 <paulo9241@gmail.com>
2025-07-14 22:04:59 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,5 @@
+var data = [
+  { Nome:"CRAS Bela Vista", Tipo:"CRAS", Latitude:"-2.53812", Longitude:"-44.28267", Municipio:"São Luís" },
+  { Nome:"CREAS Central", Tipo:"CREAS", Latitude:"-2.54500", Longitude:"-44.28000", Municipio:"São Luís" },
+  { Nome:"SSAA Povoado X", Tipo:"SSAA", Latitude:"-4.87231", Longitude:"-46.00892", Municipio:"Arame" }
+];
